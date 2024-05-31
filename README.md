4db2364b264ef181d0aaa932630bc295c3cb6a30
Scott Canaga <scottcanaga@gmail.com>
2024-05-31 16:10:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+Subsonic Guarddog
+
+The intent of this program is to watch log files for strange activity and send notifcations and handle
+
+Required libraries:
+
+psutil
+schedule
+
+Setup instructions:
+
+Enter things you want to match against in prohibited_words.txt
+
+A Gmail app password is required, set this up:
+
+Enable "Less secure app access" or Generate an App Password:
+
+Less Secure App Access: Go to your Google Account settings. Navigate to "Security," scroll down to "Less secure app access," and turn it on.
+App Password (recommended if you have 2-factor authentication enabled):
+Go to your Google Account settings.
+Navigate to the "Security" section and ensure 2-factor authentication is enabled.
+In the "Signing in to Google" section, select "App passwords."
+Follow the prompts to generate an app password for "Mail."
+
+Create environment variable 
+Open System Properties:
+
+Press Win + R, type sysdm.cpl, and press Enter.
+Go to the "Advanced" tab and click on "Environment Variables".
+
+In the Environment Variables window, under the "User variables" section, click "New".
+
+Set the variable name and value:
+
+Variable name: EMAIL_PASSWORD
+Variable value: your-app-password
+Click "OK" to close the dialogs.
---
+++ guarddog.py
@@ -0,0 +1,89 @@
+import configparser
+import smtplib
+import os
+import time
+from email.message import EmailMessage
+import psutil
+import schedule
+
+
+config_file = 'config.cfg'
+prohibited_words_file = 'prohibited_words.txt'
+
+
+def load_config():
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config
+
+
+def load_prohibited_words():
+    with open(prohibited_words_file, 'r') as file:
+        return [line.strip() for line in file if line.strip()]
+
+
+def send_email(subject, body):
+    config = load_config()
+    to_email = config.get('Email', 'email_to')
+    from_email = config.get('Email', 'email_from')
+    from_password = os.getenv('EMAIL_PASSWORD')
+
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = to_email
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(from_email, from_password)
+        server.send_message(msg)
+
+    print(f'Email sent to {to_email}')
+
+
+def guard_dog():
+    prohibited_words = load_prohibited_words()
+    config = load_config()
+    watched_file = config.get('System','watched_file')
+    guarded_app = config.get('System', 'guarded_app')
+    with open(watched_file, 'r') as file:
+        log_contents = file.readlines()
+    matched_lines = []
+    for line in log_contents:
+        for word in prohibited_words:
+            if word in line:
+                matched_lines.append(line.strip())
+                break
+    if matched_lines:
+        for proc in psutil.process_iter():
+            if proc.name() == guarded_app:
+                try:
+                    proc.terminate()
+                    print(f"Process terminated: {proc.name()}")
+                    terminated = True
+                except Exception as e:
+                    print(f"Failed to kill process: {proc.name()} {e}")
+
+        subject = f'Guarddog alert in watched file'
+        body = f'Checked activity in watched file: {watched_file}\n\n{matched_lines}\n\n' \
+               f'Guarddog {"killed guarded app" if terminated else "was not able to kill guarded app."}'
+
+        try:
+            send_email(subject, body)
+            exit(1)
+        except Exception as e:
+            print(f"Failed to send email {e}")
+
+
+def run_app():
+    config = load_config()
+    interval = int(config.get('App', 'check_interval_minutes'))
+    schedule.every(interval).minutes.do(guard_dog)
+    print(f"Guarddog started for {config.get('System', 'guarded_app')}\n"
+          f"Checking {config.get('System','watched_file')} every {interval} minutes")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+run_app()
